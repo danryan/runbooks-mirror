c06628d7fcb0e0a76b3b0c7f61453279099450c6
Michal Wasilewski <mwasilewski@gitlab.com>
2019-11-26 18:36:36 +0100

    prevent globbing and word splitting

--- elastic/managed-objects/log_gprd/index-templates/update-index-templates.sh
+++ elastic/managed-objects/log_gprd/index-templates/update-index-templates.sh
@@ -31,5 +31,5 @@ indices=(
 env=gprd
 
 for index in "${indices[@]}"; do
-    ES7_index-template_exec_jsonnet_and_upload_json $template_name $index $env
+    ES7_index-template_exec_jsonnet_and_upload_json "$template_name" "$index" "$env"
 done
